afb5737f456042459c2bcace80f6bcd3699eb927
전강현 <76657990+skchqhdpdy@users.noreply.github.com>
2022-06-19 23:40:51 +0900

    Add files via upload

--- login.js
+++ login.js
@@ -2,18 +2,27 @@ const getUserName = document.querySelector("#username input");
 const form = document.querySelector("#username");
 const sayHello = document.querySelector("#sayhello");
 
-function loginBtnClick(event) {
-    event.preventDefault();
-    const username = getUserName.value;
-    getUserName.value = "";
+function btnClick(event) {
+        event.preventDefault();
+    
+        const username = getUserName.value;
+        getUserName.value = "";
 
-    localStorage.setItem("username", username);
-    form.classList.toggle("hiddne");
+        localStorage.setItem("username", username);
+        form.classList.toggle("hiddne");
 
-    sayHello.innerText = `Hello! ${username}!`;
-    sayHello.classList.toggle("hiddne");
+        sayHello.innerText = `Hello! ${username}!`;
+        sayHello.classList.toggle("hiddne");
 
-    document.getElementById("todolist").classList.toggle("hiddne")
+        document.getElementById("inputtodolist").classList.toggle("hiddne")
 }
 
-form.addEventListener("submit", loginBtnClick);
+if (localStorage.getItem("username") !== null) {
+    form.classList.toggle("hiddne");
+    sayHello.innerText = `Hello! ${localStorage.getItem("username")}!`;
+    sayHello.classList.toggle("hiddne");
+
+    document.getElementById("inputtodolist").classList.toggle("hiddne");
+}
+
+form.addEventListener("submit", btnClick);
--- todolist.js
+++ todolist.js
@@ -1,9 +1,25 @@
-const todo = document.querySelector("#todolist");
-
+const todo = document.querySelector("#inputtodolist");
+const getTodo = document.querySelector("#inputtodolist input");
 
 function todolistBtnClick(event) {
     event.preventDefault();
+    const todolist = getTodo.value;
+    getTodo.value = "";
 
+    if (localStorage.getItem("listcount") === null || localStorage.getItem("listcount") === undefined) {
+        localStorage.setItem("listcount", 0)
+    }/////////////////////////
+    const count = parseInt(localStorage.getItem("listcount"));
+    localStorage.setItem("listcount", count + 1)
+
+    localStorage.setItem(`todolist${localStorage.getItem("listcount")}`, todolist);
+
+    const addDiv = document.createElement("div");
+    const addTodolist = document.createElement("h1");
+    addDiv.appendChild(addTodolist);
+    document.body.appendChild(addDiv);
+
+    
 }
 
 todo.addEventListener("submit", todolistBtnClick);
