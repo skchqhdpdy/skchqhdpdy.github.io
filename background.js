99110efa50b8a96aaa5c601f8601570f7d51e2db
전강현 <76657990+skchqhdpdy@users.noreply.github.com>
2022-06-19 21:41:11 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -14,22 +14,26 @@ const clockColor = document.querySelector("#clock h1")
 switch (bgImg[randomNum]) {
     case "win-10-dark.jpg":
         clockColor.style.color = "#C0C0C0";
-        
+        localStorage.setItem("backgroud Image", "win-10-dark.jpg");
         break;
 
     case "win-10-purple.jpg":
         clockColor.style.color = "yellow";
+        localStorage.setItem("backgroud Image", "win-10-purple.jpg");     
         break;
 
     case "win-10-red.jpg":
         clockColor.style.color = "aqua";
+        localStorage.setItem("backgroud Image", "win-10-red.jpg");     
         break;
 
     case "win-10.jpg":
         clockColor.style.color = "#ff7f00";
+        localStorage.setItem("backgroud Image", "win-10.jpg");     
         break;
             
     default:
         clockColor.style.color = "violet"
+        localStorage.setItem("backgroud Image", "win-xp.jpg");     
         break;
 }
--- login.js
+++ login.js
@@ -1,10 +1,42 @@
 const getUserName = document.querySelector("#username input");
 const form = document.querySelector("#username")
+const sayHello = document.querySelector("#sayhello")
+form.style.placeholderColor = "yellow"
+
+switch (localStorage.getItem("backgroud Image")) {
+    case "win-10-dark.jpg":
+        form.style.placeholderColor = "yellow"
+        break;
+
+    case "win-10-purple.jpg":
+        getUserName.style.placeholderColor = "yellow"   
+        break;
+
+    case "win-10-red.jpg":
+        getUserName.style.placeholderColor = "yellow"   
+        break;
+
+    case "win-10.jpg":
+        getUserName.style.placeholderColor = "yellow"   
+        break;
+            
+    default:
+        getUserName.style.placeholderColor = "yellow" 
+        break;
+}
 
 function btnClick(event) {
     event.preventDefault();
     const username = getUserName.value;
-    alert(username);
+    getUserName.value = "";
+
+    localStorage.setItem("username", username);
+    form.classList.toggle("hiddne");
+
+    sayHello.innerText = `Hello! ${username}!`;
+    sayHello.classList.toggle("hiddne");
+
+    document.getElementById("todolist").classList.toggle("hiddne")
 }
 
-form.addEventListener("click", btnClick)
+form.addEventListener("submit", btnClick);
